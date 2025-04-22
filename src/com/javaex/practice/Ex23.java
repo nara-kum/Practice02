53a993ff777e51896e6c4ae53d90bd049c85e8c6
nara-kum <0603skfk@gmail.com>
2025-04-22 13:44:34 +0900

    ex23 수정

--- src/com/javaex/practice/Ex23.java
+++ src/com/javaex/practice/Ex23.java
@@ -10,10 +10,12 @@ public class Ex23 {
 		
 		System.out.println("태어난 년도를 입력해주세요");
 		System.out.print("년도 : ");
+		
 		int boarn = sc.nextInt();
 		int year = 2025;
 		System.out.println("올해 : "+ year);
 		System.out.println("태어난 해 : "+ boarn);
+		
 		int age = 2025-boarn;
 		System.out.println("나이 : "+ age);
 		
@@ -21,19 +23,16 @@ public class Ex23 {
 		
 		if(age>=20) {
 			System.out.println("20세 이상");
-			if(boarn%2==0&&year%2==0) {
+			
+			if((boarn%2==0&&year%2==0)||(boarn%2==1&&year%2==1)) {
 				System.out.println("건강검진해");
+				
 				if(age>=40) {
 					System.out.println("암 검사");
+				}else{
+					System.out.println("암 검사X");
 				}
-			}else if (boarn%2==0&&year%2!=0) {
-				System.out.println("건강검진해 아님");
-			}else if (boarn%2==1&&year%2==1) {
-				System.out.println("건강검진해");
-				if(age>=40) {
-					System.out.println("암 검사");
-				}
-			}else if (boarn%2==0&&year%2!=1) {
+			}else if ((boarn%2==0&&year%2!=0)||(boarn%2==1&&year%2!=1)) {
 				System.out.println("건강검진해 아님");
 			}
 		}else {
